258452221556d6a3f0eeb8025feba6465d4c61fd
Zoe Ames <zoeames@gmail.com>
2021-05-22 21:15:11 -0500

    styling correct for drop card

--- client/src/containers/LegoCardContainer/LegoCardContainer.js
+++ client/src/containers/LegoCardContainer/LegoCardContainer.js
@@ -8,7 +8,7 @@ class LegoCardContainer extends React.Component {
     const { sets, openBigCard, selectedSet } = this.props;
     return (
       <div className="LegoCardContainer">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-4">
             {sets.map((set) => <LegoCard key={set.id} set={set} openBigCard={openBigCard} selectedSet={selectedSet}/>)}
         </div>
       </div>
